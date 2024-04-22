f44b20da08bb1755381ad0ee38035e8131d82a4d
harvey bal <harvey.bal@sky.uk>
2024-04-22 11:27:04 +0100

    added dashbaord and moved to seperate namespaces

---
+++ dashboard/apply.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+
+kubectl apply -f ./k8_resources
+kubectl -n kubernetes-dashboard create token admin-user
+kubectl apply -f ./k8_resources
+kubectl get secret admin-user -n kubernetes-dashboard -o jsonpath={".data.token"} | base64 -d
+
+sleep 10
+kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443
--- pong_service/apply.sh
+++ pong_service/apply.sh
@@ -3,4 +3,5 @@
 docker build . -t docker.io/library/pong:v1
 kind load docker-image docker.io/library/pong:v1 --name grpc-cluster
 
+kubectl apply -f ./k8_resources/namespace.yml
 kubectl apply -f ./k8_resources
